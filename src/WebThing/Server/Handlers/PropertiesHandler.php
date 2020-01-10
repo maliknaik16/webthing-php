5a604cf723ea15b4c0cf7f2e1877420b55f84609
Malik Naik <maliknaik16@gmail.com>
2020-01-10 13:41:35 +0530

    Add PropertiesHandler.php

---
+++ src/WebThing/Server/Handlers/PropertiesHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace WebThing\Server\Handlers;
+
+/**
+ * @file
+ * PropertiesHandler class implementation.
+ */
+
+use WebThing\Thing;
+
+/**
+ * Handle a request to '/properties'.
+ */
+class PropertiesHandler extends BaseHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function get() {
+    $thing_id = isset($this->getRouteArgs()['thing_id']) ?: '0';
+    $thing = $this->getThing($thing_id);
+
+    if($thing == NULL) {
+      $this->sendError(404);
+      return;
+    }
+
+    $this->setContentType('application/json');
+    $this->write(json_decode($thing->getProperites()));
+  }
+}
