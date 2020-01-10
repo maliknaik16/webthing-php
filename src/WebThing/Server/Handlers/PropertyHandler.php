ecade7fb87d468987ce6e7fde23d4e6df005593d
Malik Naik <maliknaik16@gmail.com>
2020-01-10 14:54:47 +0530

    Add PropertyHandler.php

---
+++ src/WebThing/Server/Handlers/PropertyHandler.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace WebThing\Server\Handlers;
+
+/**
+ * @file
+ * PropertyHandler class implementation.
+ */
+
+/**
+ * Handle a request to '/properties/<property>'.
+ */
+class PropertyHandler extends BaseHandler {
+
+  /**
+   * The thing from the ID of the URL path.
+   *
+   * @var WebThing\ThingInterface
+   */
+  protected $thing;
+
+  /**
+   * Name of the property from the URL path.
+   *
+   * @var mixed
+   */
+  protected $property_name;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function initialize() {
+    $thing_id = isset($this->getRouteArgs()['thing_id']) ?: '0';
+
+    $this->thing = $this->getThing($thing_id);
+    $this->property_name = isset($this->getRouteArgs()['property_name']) ?: NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function get() {
+
+    if($this->thing == NULL) {
+      $this->sendError(404);
+      return;
+    }
+
+    if($this->thing->hasProperty($this->property_name)) {
+      $this->setContentType('application/json');
+      $this->write(json_decode([
+        $this->property_name: $this->thing->getProperty($this->property_name),
+      ]));
+    }else{
+      $this->sendError(404);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function put() {
+
+    if($this->thing == NULL) {
+      $this->sendError(404);
+      return;
+    }
+
+    $args = json_decode($this->getRequest()->getParsedBody());
+
+    if($args === NULL && json_last_error() !== JSON_ERROR_NONE) {
+      $this->setStatus(400);
+      return;
+    }
+
+    if(!in_array($this->property_name, $args)) {
+      $this->setStatus(400);
+      return;
+    }
+
+    if($this->thing->hasProperty($this->property_name)) {
+      try {
+        $this->thing->setProperty($this->property_name, $args[$this->property_name]);
+      } catch(\Exception $e) {
+        $this->setStatus(400);
+        return;
+      }
+
+      $this->setContentType('application/json');
+      $this->write(json_decode([
+        $this->property_name: $this->thing->getProperty($this->property_name),
+      ]));
+
+    } else {
+      $this->setStatus(400);
+    }
+  }
+}
