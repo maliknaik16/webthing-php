95f9c89349170918751e4b8b352751e5db0659bc
Malik Naik <maliknaik16@gmail.com>
2020-01-15 17:53:35 +0530

    Add Travis CI

---
+++ travis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+git clone https://github.com/maliknaik16/webthing-php.git
+cd webthing-php
+
+# clone the webthing-tester
+git clone https://github.com/mozilla-iot/webthing-tester
+pip3 install --user -r webthing-tester/requirements.txt
+
+composer install
+
+export PHP_PATH=.
+# build and test the single-thing example
+php examples/single-thing.php &
+EXAMPLE_PID=$!
+sleep 5
+./webthing-tester/test-client.py --protocol http --host localhost --port 8888 --debug
+kill -15 $EXAMPLE_PID
+
+# build and test the multiple-things example
+php examples/multiple-things.php &
+EXAMPLE_PID=$!
+sleep 5
+./webthing-tester/test-client.py --protocol http --host localhost --port 8080 --path-prefix "/0" --debug
+kill -15 $EXAMPLE_PID
